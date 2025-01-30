bef73050cff7f5386820753046c214be89504225
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-30 08:25:25 +0100

    Wrapped the app in an auth provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "../context/ThemeContext";
 import "../global.css";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <AuthProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
